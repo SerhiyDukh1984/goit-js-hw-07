a5e3f6af01848bfcfbead0a7e71c9b26de0e0edd
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-28 20:11:18 +0300

    finished 7.02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+console.log(
+  "🚀 ~ file: 02-lightbox.js ~ line 5 ~ galleryContainer",
+  galleryContainer
+);
+const imageEl = createImageMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imageEl);
+galleryContainer.addEventListener("click", onGaleryContainerClick);
+
+function createImageMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+        `;
+    })
+    .join("");
+}
+
+function onGaleryContainerClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionPosition: "bottom",
+    captionsData: "alt",
+    navText: ["<~", "~>"],
+  });
+}
