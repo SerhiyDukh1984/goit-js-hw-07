471218552eaff24e318bd45a7e180213b20613ee
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-28 20:11:00 +0300

    finished 7.01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,26 +3,39 @@ import { galleryItems } from "./gallery-items.js";
 
 const galleryContainer = document.querySelector(".gallery");
 const imageEl = createImageMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", imageEl);
-console.log(
-  "🚀 ~ file: 01-gallery.js ~ line 7 ~ galleryContainer",
-  galleryContainer
-);
-console.log(galleryContainer);
 
-galleryContainer.addEventListener("click", getOriginalImage);
+galleryContainer.insertAdjacentHTML("beforeend", imageEl);
+
+galleryContainer.addEventListener("click", onGaleryContainerClick);
 
 function createImageMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
       return `
-    <div class =' image-card' >
-      <img class = 'original' src = '${original}' alt = '${description}'>
-      <img class = 'preview' src = '${preview}' alt = '${description}'>     
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
     </div>
         `;
     })
     .join("");
 }
 
-function getOriginalImage(event) {}
+function onGaleryContainerClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imageRef = e.target.dataset.source;
+  const removeImageRef = basicLightbox.create(`<img src="${imageRef}"/>`);
+
+  removeImageRef.show();
+}
--- js/modal.js
+++
@@ -1,7 +0,0 @@
-import * as basicLightbox from "basiclightbox";
-
-const instance = basicLightbox.create(`
-    <img src="">
-`);
-
-instance.show();
